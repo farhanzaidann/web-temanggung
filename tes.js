7e787bda67accd792ecfc55b40ae82352a348e49
farhanzaidann <farhnzaidan@gmail.com>
2022-06-28 20:32:37 +0700

    fitur baru

--- tes.js
+++ tes.js
@@ -5,3 +5,17 @@ function myFunction() {
     let alertMessage = `Selamat Datang ${nama}, Email : ${email}, Message : ${message}`
     alert(alertMessage)
 }
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    
+}))
